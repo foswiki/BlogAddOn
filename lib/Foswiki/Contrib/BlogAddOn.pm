7896faff1f181a4133a6879bfcbe0250ce775991
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-02-20 14:13:44 +0000

    Item10378: make sure the existing value of Category field is not lost
    git-svn-id: http://svn.foswiki.org/trunk/BlogAddOn@10745 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/BlogAddOn.pm
+++ lib/Foswiki/Contrib/BlogAddOn.pm
@@ -9,7 +9,7 @@ our $VERSION = '$Rev$';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-our $RELEASE = '1.6.0';
+our $RELEASE = '1.6.1';
 
 our $SHORTDESCRIPTION = 'A simple weblog application based on Data Forms';
 
